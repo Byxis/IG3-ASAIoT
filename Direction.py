826c7b44ddb96a6d64d58cbd252e2a826ad997f8
alexis.serrano <alexis.serrano@etu.umontpellier.fr>
2025-01-15 12:54:08 +0100

    refractor: Reorganisation of the code and added comments

--- Direction.py
+++
@@ -1,5 +0,0 @@
-from enum import Enum
-
-class Direction(Enum):
-    LEFT = 0
-    RIGHT = 1
--- WasteType.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class WasteType(Enum):
-    RECYCLABLE = 'Recyclable'
-    BIODEGRADABLE = 'Biodegradable'
-    NON_RECYCLABLE = 'Non-Recyclable'
-    GLASS = "Glass"
-    COMPOSED = "Composed"
-
