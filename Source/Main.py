06974ea89033ed7002e59301a4c20fac5d08304c
sebastien.pinta <sebastien.pinta@etu.umontpellier.fr>
2025-01-27 11:32:39 +0100

    small fix

--- Source/Main.py
+++ Source/Main.py
@@ -1,6 +1,5 @@
 from Class.Game import Game
 
 if __name__ == "__main__":
-    #game = Game(input("Enter your name: "))
-    game = Game("Joueur")
+    game = Game(input("Enter your name: "))
     game.play()
